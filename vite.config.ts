9bfe0a735a78a7494b223b4a220428244ea0a059
gfwx <gfawkes1832@gmail.com>
2025-12-26 10:23:46 +0530

    updated PWA register

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import * as path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       // Alias @ to the src directory
